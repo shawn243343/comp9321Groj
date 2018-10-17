5de6f735c48af4b3f80735641171b91ccaa95f6f
anhao0522 <39584855+anhao0522@users.noreply.github.com>
2018-10-17 17:53:36 +1100

    Update train_model_v2.py

--- train_model_v2.py
+++ train_model_v2.py
@@ -13,9 +13,10 @@ loaded_data = pd.read_csv("train_data_v4.csv")
 data_x = loaded_data[["country","winery","variety"]]
 #data_x = std.fit_transform(data_x)
 data_y = loaded_data[["price"]]
-x_train, x_test, y_train, y_test = train_test_split(data_x,data_y, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(data_x,data_y, test_size = 0.2,random_state = 14)
 
 #bulid model
+#fit_intercept=False
 model = LinearRegression(fit_intercept=False)
 model.fit(x_train, y_train)
 
@@ -30,6 +31,7 @@ MSE2=metrics.mean_squared_error(data_y,predicted)
 #data visualization
 plt.scatter(data_y, predicted, color='y', marker='o')
 plt.scatter(data_y, data_y,color='g', marker='+')
+plt.savefig('./plot.jpg')
 plt.show()
 
 a1=model.coef_[0][0]
@@ -46,8 +48,9 @@ def show_MSE():
     return MSE1,MSE2
 
 def get_weights():
-    return [a1,a2,a3]
+    return [a1,a2,a3,b]
 
 print(MSE1,MSE2)
 print(a1,a2,a3)
 #print(predict_price(79,39,77))
+
