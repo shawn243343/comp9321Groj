637112d86a3b11013a93c81635cad6fcc9ff737a
anhao0522 <39584855+anhao0522@users.noreply.github.com>
2018-10-17 17:20:25 +1100

    Add files via upload

---
+++ rank.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+
+def ranked(country, variety, price, num):
+    data = pd.read_csv("wine_final.csv")
+    if num == 0:
+        return None
+    if country !='':
+        data = data.query("country==\"{}\"".format(country))
+
+    if variety !='':
+        data = data.query("variety==\"{}\"".format(variety))
+
+    if price !='':
+        # data['price']=data['price'].str.replace(',','').astype(int)
+        data = data.query("price=={}".format(price))
+    data = data.sort_values(by='points', ascending=False)
+    result=[]
+    count = 0
+    for index,row in data.iterrows():
+        if count >= num:
+            break
+        d = {"Name":{row["title"]},"Varitey": {row["variety"]},"Price": row["price"],"Points":row["points"]}
+        count += 1
+        result.append(d)
+
+    return result
+#ranked("US","","",10)
