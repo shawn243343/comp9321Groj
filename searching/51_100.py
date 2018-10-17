8166c5d1ba87c4f7b4f18669f5272429f0bd7931
anhao0522 <39584855+anhao0522@users.noreply.github.com>
2018-10-18 00:01:26 +1100

    Add files via upload

---
+++ searching/51_100.py
@@ -0,0 +1,56 @@
+dict_palate={"weighty":["Souverain 2010 Chardonnay (North Coast)"],
+      "creamy":["Souverain 2010 Chardonnay (North Coast)","Drumheller 2014 Chardonnay (Columbia Valley (WA))","Fattoria Sardi 2015 Rosato (Toscana)"],
+      "medium":["Souverain 2010 Chardonnay (North Coast)","Mitolo 2016 Jester Sangiovese Rosé (McLaren Vale)"],
+        "crisp":["Tasca d'Almerita 2011 Sallier de la Tour Grillo (Sicilia)","Mitolo 2016 Jester Sangiovese Rosé (McLaren Vale)","Henry Fessy 2015  Régnié"],
+        "simple":["Tasca d'Almerita 2011 Sallier de la Tour Grillo (Sicilia)","Podere dal Nespoli 2015 Prugneto Sangiovese (Romagna)"],
+        "light":["Tres Palacios 2011 Reserve Pinot Noir (Maipo Valley)","Drumheller 2014 Chardonnay (Columbia Valley (WA))","Hindsight 2012 Estate Grown Petite Sirah (Calistoga)","Spyro 2014 Albariño (Rías Baixas)","Lionel Osmin & Cie 2016 La Réserve Petit Manseng (Vin de France)","Passaggio 2014 Blau Vineyards Merlot (Knights Valley)","Henry Fessy 2015  Régnié"],
+        "juicy":["Tres Palacios 2011 Reserve Pinot Noir (Maipo Valley)"],
+        "dry":["Tres Palacios 2011 Reserve Pinot Noir (Maipo Valley)","Palencia 2016 Albariño (Ancient Lakes)"],
+        "sweet":["Okapi 2013 Estate Cabernet Sauvignon (Napa Valley)","Lionel Osmin & Cie 2016 La Réserve Petit Manseng (Vin de France)"],
+        "soft":["Vignerons des Terres Secrètes 2015  Mâcon-Milly Lamartine","Schmitt Söhne 2015 Riesling (Rheinhessen)","Yalumba 2016 Made With Organic Grapes Chardonnay (South Australia)"],
+        "smoothy":["Vignerons des Terres Secrètes 2015  Mâcon-Milly Lamartine","Madonna Alta 2014 Nativo Red (Toscana)"],
+        "salty":["Aresti 2014 Special Release Reserva Carmenère (Rapel Valley)"],
+        "spicy":["Lionel Osmin & Cie 2016 La Réserve Petit Manseng (Vin de France)"]}
+
+
+dict_flavor={"lime":["Souverain 2010 Chardonnay (North Coast)"],
+             "pear":["Souverain 2010 Chardonnay (North Coast)","Vignerons des Terres Secrètes 2015  Mâcon-Milly Lamartine","Palencia 2016 Albariño (Ancient Lakes)"],
+             "almond":["Tasca d'Almerita 2011 Sallier de la Tour Grillo (Sicilia)"],
+             "citrus":["Tasca d'Almerita 2011 Sallier de la Tour Grillo (Sicilia)","Schmitt Söhne 2015 Riesling (Rheinhessen)"],
+             "lemon":["Tasca d'Almerita 2011 Sallier de la Tour Grillo (Sicilia)"],
+            "berry":["Tres Palacios 2011 Reserve Pinot Noir (Maipo Valley)","Fattoria Sardi 2015 Rosato (Toscana)","Tres Palacios 2011 Reserve Pinot Noir (Maipo Valley)","Hindsight 2013 Bella Vetta Vineyard Cabernet Sauvignon (Howell Mountain)","Henry Fessy 2015  Régnié"],
+             "vanilla":["Souverain 2010 Chardonnay (North Coast)","Okapi 2013 Estate Cabernet Sauvignon (Napa Valley)","Podere dal Nespoli 2015 Prugneto Sangiovese (Romagna)","Drumheller 2014 Chardonnay (Columbia Valley (WA))","Aresti 2014 Special Release Reserva Carmenère (Rapel Valley)"],
+             "plum":["Podere dal Nespoli 2015 Prugneto Sangiovese (Romagna)","Okapi 2013 Estate Cabernet Sauvignon (Napa Valley)","Hindsight 2013 Bella Vetta Vineyard Cabernet Sauvignon (Howell Mountain)","Hindsight 2012 Estate Grown Petite Sirah (Calistoga)","Aresti 2014 Special Release Reserva Carmenère (Rapel Valley)"],
+             "pineapple":["Yalumba 2016 Made With Organic Grapes Chardonnay (South Australia)","Lionel Osmin & Cie 2016 La Réserve Petit Manseng (Vin de France)"]}
+             
+             
+            
+red=["Tres Palacios 2011 Reserve Pinot Noir (Maipo Valley)","Okapi 2013 Estate Cabernet Sauvignon (Napa Valley)","Podere dal Nespoli 2015 Prugneto Sangiovese (Romagna)","Aresti 2014 Special Release Reserva Carmenère (Rapel Valley)","Passaggio 2014 Blau Vineyards Merlot (Knights Valley)","Madonna Alta 2014 Nativo Red (Toscana)","Henry Fessy 2015  Régnié","Hindsight 2012 Estate Grown Petite Sirah (Calistoga)","Hindsight 2013 Bella Vetta Vineyard Cabernet Sauvignon (Howell Mountain)"]
+white=["Souverain 2010 Chardonnay (North Coast)","Tasca d'Almerita 2011 Sallier de la Tour Grillo (Sicilia)","Vignerons des Terres Secrètes 2015  Mâcon-Milly Lamartine","Drumheller 2014 Chardonnay (Columbia Valley (WA))","Spyro 2014 Albariño (Rías Baixas)","Lionel Osmin & Cie 2016 La Réserve Petit Manseng (Vin de France)","Mitolo 2016 Jester Sangiovese Rosé (McLaren Vale)","Palencia 2016 Albariño (Ancient Lakes)","Fattoria Sardi 2015 Rosato (Toscana)","Schmitt Söhne 2015 Riesling (Rheinhessen)","Yalumba 2016 Made With Organic Grapes Chardonnay (South Australia)"]
+url={"Souverain 2010 Chardonnay (North Coast)":"https://www.winebid.com/Photo/Medium2x/326207",
+     "Tasca d'Almerita 2011 Sallier de la Tour Grillo (Sicilia)":"https://shop.vinicafenoir.it/226-tm_large_default/grillo-2011-sallier-de-la-tour-.jpg",
+     "Tres Palacios 2011 Reserve Pinot Noir (Maipo Valley)":"https://cdn1.wine-searcher.net/images/labels/59/22/tres-palacios-reserve-pinot-noir-maipo-valley-chile-10755922.jpg",
+     "Okapi 2013 Estate Cabernet Sauvignon (Napa Valley)":"http://shop.okapiwines.com/assets/images/products/pictures/2013OkapiCabimage1.png",
+     "Vignerons des Terres Secrètes 2015  Mâcon-Milly Lamartine":"https://kissmychef.com/wp-content/uploads/2017/06/Mâcon-Milly-Lamartine-2015-VTS.jpg",
+     "Podere dal Nespoli 2015 Prugneto Sangiovese (Romagna)":"https://cdn1.wine-searcher.net/images/labels/87/25/poderi-dal-nespoli-prugneto-sangiovese-di-romagna-emilia-romagna-italy-10928725.jpg",
+     "Drumheller 2014 Chardonnay (Columbia Valley (WA))":"https://assets.wine/files/brands_cms/BottleShot/1302/DRM_Bottle_Chard-edit_large.png",
+     "Hindsight 2013 Bella Vetta Vineyard Cabernet Sauvignon (Howell Mountain)":"http://www.hindsightwines.com/themes/hindsightwines/uploads/wines/images/vin6_screen-shot-2016-10-13-at-4.55.25-pm.png",
+     "Hindsight 2012 Estate Grown Petite Sirah (Calistoga)":"http://www.hindsightwines.com/themes/hindsightwines/uploads/wines/images/vin7_screen-shot-2016-10-13-at-5.02.11-pm.png",
+     "Schmitt Söhne 2015 Riesling (Rheinhessen)":"https://www.saratogawine.com/files/images/cached_thumbs/d9/d9b1d3c1ef97f4ea7b02b286702a3809.jpg",
+     "Yalumba 2016 Made With Organic Grapes Chardonnay (South Australia)":"https://smhttp-ssl-54521.nexcesscdn.net/media/catalog/product/cache/1/thumbnail/500x800/9df78eab33525d08d6e5fb8d27136e95/y/a/yalumba-organic-chardonnay-south-australia.jpg",
+     "Aresti 2014 Special Release Reserva Carmenère (Rapel Valley)":"https://cdn.ct-static.com/labels/1042327.jpg",
+     "Spyro 2014 Albariño (Rías Baixas)":"http://www.tastings.com/Product-Images/Wine/2016/11_8_2016/217305_fr.jpg",
+     "Lionel Osmin & Cie 2016 La Réserve Petit Manseng (Vin de France)":"https://www.lacavedescontrebandiers.fr/mediatheque/products/M1FRLOREPM1675/product/product.png",
+     "Mitolo 2016 Jester Sangiovese Rosé (McLaren Vale)":"https://cdn.shopify.com/s/files/1/1504/5726/products/2016_Mitolo_Jester_Sangiovese_Rose-20161201124203_47a53d9b-509a-4f48-aa2a-43a8419a85ad_large.png",
+     "Palencia 2016 Albariño (Ancient Lakes)":"https://palenciawine.com/wp-content/uploads/2013/09/2016-Albarino-Snow-small.png",
+     "Passaggio 2014 Blau Vineyards Merlot (Knights Valley)":"https://scontent-sjc3-1.cdninstagram.com/vp/a232f55b6ac72efaea7a9ef447bcf585/5C4DEEF2/t51.2885-15/e35/41158158_2128469673891604_5838777128451381850_n.jpg",
+     "Fattoria Sardi 2015 Rosato (Toscana)":"https://cdn11.bigcommerce.com/s-e6b77/images/stencil/1280x1280/products/14287/14659/fattoria-sardi-toscana-rose-igt__47449.1496359415.jpg",
+     "Henry Fessy 2015  Régnié":"https://www.louislatour.co.uk/upload/wine_image/overview/Fessy_Château%20des%20Reyssiers%20bottle%20Overview.png",
+     "Madonna Alta 2014 Nativo Red (Toscana)":"http://www.globalwinemeetings.com/products/_images/fondo_del_sole/fondo_del_sole-salento-madonna.png"}
+     
+
+
+
+
+
+     
